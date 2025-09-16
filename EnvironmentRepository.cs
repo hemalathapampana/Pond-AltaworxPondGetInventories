331faace04383f06168e4154bf9b3fc9b03d869a
hemalathapampana <137771820+hemalathapampana@users.noreply.github.com>
2025-09-16 17:43:47 +0530

    Add files via upload

---
+++ EnvironmentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.Lambda.Core;
+
+namespace Amop.Core.Repositories.Environment
+{
+    public class EnvironmentRepository : IEnvironmentRepository
+    {
+        public string GetEnvironmentVariable(ILambdaContext context, string key)
+        {
+            string value = null;
+            if (System.Environment.GetEnvironmentVariables().Contains(key))
+            {
+                value = System.Environment.GetEnvironmentVariable(key);
+            }
+
+            if (string.IsNullOrWhiteSpace(value) &&
+                context.ClientContext != null &&
+                context.ClientContext.Environment != null &&
+                context.ClientContext.Environment.ContainsKey(key))
+            {
+                context.ClientContext.Environment.TryGetValue(key, out value);
+            }
+
+            return value;
+        }
+    }
+}
